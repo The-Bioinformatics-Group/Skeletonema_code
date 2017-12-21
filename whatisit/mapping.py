31ba253c04513cf8ae9e90ebc6d7af47ee93a89e
Mats Töpel <mats@topel.se>
2017-12-21 23:07:52 +0100

    Now generate output filenaes based on the names of the input files

--- whatisit/mapping.py
+++ whatisit/mapping.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python
 
+###########################################
+# Decoding SAM flags 
+# https://broadinstitute.github.io/picard/explain-flags.html
+#
+# Example: cut -f2 out.sam | sort -u
+# 77  = read paired (0x1), read unmapped (0x4), mate unmapped (0x8), first in pair (0x40)
+# 141 = read paired (0x1), read unmapped (0x4), mate unmapped (0x8), second in pair (0x40) 
+#
+# Sum of all applicable flags. Flags relevant to Bowtie are:
+#
+# 1	  The read is one of a pair
+# 2	  The alignment is one end of a proper paired-end alignment
+# 4   The read has no reported alignments
+# 8   The read is one of a pair and has no reported alignments
+# 16  The alignment is to the reverse reference strand
+# 32  The other mate in the paired-end alignment is aligned to the reverse reference strand
+# 64  The read is mate 1 in a pair
+# 128 The read is mate 2 in a pair
+#
+# Thus, an unpaired read that aligns to the reverse reference strand will have flag 16. 
+# A paired-end read that aligns and is the first mate in the pair will have flag 83 (= 64 + 16 + 2 + 1).
+#############################################
+
 import subprocess
+import json
+import files
 
 REF = "References"
 SAM_HEADERS = ["@HD", "@SQ", "@RG", "@PG", "@CO"]
 
 class Fastq_file(object):
 	def __init__(self, name):
-		self.name = name
+		self.name = name + ".unmapped" + ".fastq"
 		self.OUTHANDLE = open(self.name, "w")
 
 	# Takes a Fastq_read object as argument
@@ -31,13 +56,14 @@ def index(reference):
 
 # Takes a pair of fastq files and a fasta file with genome references as input
 # Returns a dictioneary with the counts of mapped reads to the individual reference "species"
-def bowtie2(fastq1, fastq2, reference):
+def bowtie2(fastq1, fastq2, reference, sam_file_name):
+	indexed_db = files.file_name_base(reference)
 	index(reference)
 	sam = subprocess.check_output(["bowtie2", "-x", REF, "-1", fastq1, "-2", fastq2])
 
 	# Count the mapped reads...
 	mapped = {}
-	unmapped_reads = Fastq_file("Unmapped.fastq")		# Devel.
+	unmapped_reads = Fastq_file(sam_file_name)		# Devel.
 	for line in sam.split("\n"):
 		# Skip SAM header lines
 		try:
@@ -47,7 +73,7 @@ def bowtie2(fastq1, fastq2, reference):
 				ref_name = line.split()[2]
 				if ref_name not in mapped:
 					mapped[ref_name] = 1
-				# ...and store unmapped reads
+				# ...and store unmapped reads in a file
 				elif ref_name == "*":
 					mapped[ref_name] += 1
 					read = Fastq_read(line.split()[0], line.split()[9], line.split()[10])
@@ -57,6 +83,7 @@ def bowtie2(fastq1, fastq2, reference):
 		except IndexError:
 			continue
 	
+	print json.dumps(mapped)
 	print mapped
 
 	
